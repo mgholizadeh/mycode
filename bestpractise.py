9fd5cd3867d7b1e9ca30821bf295f4054a2bab48
mo0o0o0 <mo0o0o0@users.noreply.github.com>
2021-12-13 20:16:24 +0000

    second push

---
+++ bestpractise.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+def main():
+
+    my_string = "your code would go"
+    print(my_string)
+
+
---
+++ lab_input/iptaker03.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   print() - concatenate vs print a series of items"""
+def main():
+   user_input = input("what is your ip address?:")
+   print("you ttold me that ipv4 address is:" + user_input)
+   vendor= input("what is the vendor name?:")
+   print(vendor)
+   user_price = input("how much does it cost?:")
+   print("you said the price is:" , user_price)
+main()
