c6cce56dfd4fcf8950d6c6744ee74f27a3aa99f6
mo0o0o0 <mo0o0o0@users.noreply.github.com>
2021-12-13 21:42:48 +0000

    sync

---
+++ mix-list/chaleneg01_mixlist.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+print("ip addresses are:" , iplist[3] , "and",  iplist[4])
+print(f"ports are:{iplist[1]}, and {iplist[2]}")
---
+++ mix-list/mixlist01.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+my_list = ["192.168.10.1" , 5060 , "up"]
+print("the first item in the list is (ip): " + my_list[0])
+print("the seond item n the list is (port): " + str(my_list[1]) )
+print("The last item in the list (state): " + my_list[2] )
+    
